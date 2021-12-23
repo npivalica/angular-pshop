958c11e61e7bc8a65f2f010886d16142f5f2f991
[Nikolina Pivalica] <[nikolina.pivalica@gmail.com]>
2021-12-23 21:42:26 +0100

    Added Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDZV2hNXtpjSCR7MSu9MJWOcGti9gdyXaA",
+    authDomain: "pshop-64c1c.firebaseapp.com",
+    projectId: "pshop-64c1c",
+    storageBucket: "pshop-64c1c.appspot.com",
+    messagingSenderId: "762874737992",
+    appId: "1:762874737992:web:6c035c8293f9bd1ff86f7d",
+    measurementId: "G-DQ91EJBSMM"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDZV2hNXtpjSCR7MSu9MJWOcGti9gdyXaA",
+    authDomain: "pshop-64c1c.firebaseapp.com",
+    projectId: "pshop-64c1c",
+    storageBucket: "pshop-64c1c.appspot.com",
+    messagingSenderId: "762874737992",
+    appId: "1:762874737992:web:6c035c8293f9bd1ff86f7d",
+    measurementId: "G-DQ91EJBSMM"
+  }
 };
 
 /*
