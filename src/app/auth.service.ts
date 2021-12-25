c16f818127ee7d6d9d2d1c258384685bed96e5c2
[Nikolina Pivalica] <[nikolina.pivalica@gmail.com]>
2021-12-25 22:29:05 +0100

    fixing the bug with logout

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,5 @@
 import { UserService } from './user.service';
-import { Observable } from 'rxjs';
+import { Observable, of} from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/auth';
@@ -7,31 +7,35 @@ import { ActivatedRoute } from '@angular/router';
 import { AppUser } from './models/app-user';
 import { switchMap, map } from 'rxjs/operators';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth : AngularFireAuth, private route: ActivatedRoute, private userService: UserService) {
+  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute, private userService: UserService) {
     this.user$ = afAuth.authState;
-   }
+  }
 
-    login(){
-      let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
-      localStorage.setItem('returnUrl', returnUrl);
-      this.afAuth.signInWithRedirect(new firebase.GoogleAuthProvider())
-    }
+  login() {
+    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    localStorage.setItem('returnUrl', returnUrl);
+    this.afAuth.signInWithRedirect(new firebase.GoogleAuthProvider())
+  }
 
-    logout(){
-      this.afAuth.signOut();
-    }
+  logout() {
+    this.afAuth.signOut();
+  }
 
-    get appUser$() : Observable<AppUser> {
-      return this.user$
-        .pipe(switchMap(user => this.userService.get(user.uid)))
+  get appUser$(): Observable<any> {
+    return this.user$
+      .pipe(switchMap(user => {
+        if (user) return this.userService.get(user.uid);
+        return of(null)
+      }))
 
-    }
+  }
 }
 
 
