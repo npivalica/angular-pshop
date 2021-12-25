1fdf41192641c58d8e84d276495d63a5735295cd
[Nikolina Pivalica] <[nikolina.pivalica@gmail.com]>
2021-12-25 12:59:05 +0100

    login & logout + display name of the user

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,5 +1,7 @@
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/auth';
+
 
 @Component({
   selector: 'bs-navbar',
@@ -7,9 +9,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent {
-
+  user: firebase.User;
   constructor(private afAuth: AngularFireAuth) {
-    afAuth.authState.subscribe(x => console.log(x));
+    afAuth.authState.subscribe(user => this.user = user);
   }
 
   logout(){
